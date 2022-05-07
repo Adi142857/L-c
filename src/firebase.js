a02856aaa5f1aecf6a81688aee96b51f10d76c79
Aditya <65461420+Adi142857@users.noreply.github.com>
2022-05-07 19:30:43 +0530

    env

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCaxFx3oWnaKDs0gbrlZuxxff8CSwETlv8",
-    authDomain: "lin-clone.firebaseapp.com",
-    projectId: "lin-clone",
-    storageBucket: "lin-clone.appspot.com",
-    messagingSenderId: "1082220803996",
-    appId: "1:1082220803996:web:472b3e4d18f83426aa3a59"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   
   const firebaseApp=firebase.initializeApp(firebaseConfig);
